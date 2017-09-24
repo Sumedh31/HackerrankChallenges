6cf5b5ec7254757d3fdde3b4edf77c8d2e2ca92f
sumedh.tambe <sumedh.tambe@adweb.com.au>
2017-09-24 12:21:04 +0530

    Solved nested list second lower challenge

--- src/NestedLists.py
+++ src/NestedLists.py
@@ -1,37 +1,28 @@
-'''
-Created on 12-Sep-2017
-
-@author: Sumedh.Tambe
-'''
 from pip._vendor.distlib.compat import raw_input
-
-
 list1=[]
 
 for i in range(int(raw_input())):
     name=raw_input()
     score=float(raw_input())    
     list1.append([name,score])
+    
 lowest=float("inf") 
 secondlowest=float("inf")
+
 for index, i in enumerate(range(len(list1))):
-    if(format(float(list1[index][1]),'.2f')<format(float(secondlowest),'.2f')):
-        if(format(float(list1[index][1]),'.2f')<format(float(lowest),'.2f')):
-            lowest,secondlowest=format(float(list1[index][1]),'.2f'),format(float(lowest),'.2f')
+    if(format(float(list1[index][1]),'.4f')<format(float(secondlowest),'.4f')):
+        if(format(float(list1[index][1]),'.4f')<format(float(lowest),'.4f')):
+            lowest,secondlowest=format(float(list1[index][1]),'.4f'),format(float(lowest),'.4f')
         else:
-            secondlowest=format(float(list1[index][1]),'.2f')
+            secondlowest=format(float(list1[index][1]),'.4f')
 
 #print(list1[index][0] for index, i in enumerate(range(len(list1))) if(float(list1[index][1])==float(secondlowest)))
- 
+list2=[] 
 for index, i in enumerate(range(len(list1))):
-    if(format(float(list1[index][1]),'.2f')==format(float(secondlowest),'.2f')):
-        list2=[]
+    if(format(float(list1[index][1]),'.4f')==format(float(secondlowest),'.4f')):
         list2.append((list1[index][0]))
-        #list2=list2.sort()
-        list2.sort()
-        print(list2)
-         
-#     
+        
+list2=sorted(list2,key=str.lower)
     
-    
-#print(list1) 
+for index,i in enumerate(range(len(list2))):
+    print(list2[index]) 
